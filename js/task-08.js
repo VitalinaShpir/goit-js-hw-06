1a2242edf75824e6f3ebea52d40ac734051944a5
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-04-30 19:36:31 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onLogin);
+
+function onLogin(event) {
+    event.preventDefault();
+    const userRef = {
+        email: event.currentTarget.elements.email.value,
+        password: event.currentTarget.elements.password.value,
+    }
+    if (userRef.email === '' || userRef.password === '') {
+        alert('Please, enter all fields in form');
+        return;
+    }
+    userRef.password = userRef.password.replaceAll(' ', '');
+    console.log(userRef);
+    event.currentTarget.reset();
+};
