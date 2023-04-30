7dc4d87e5ad9fbe5ce39436b645f81f242486f98
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-04-30 19:38:49 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const changeColorBtn = document.querySelector('.change-color');
+const txtColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newChangeColor = getRandomHexColor();
+  document.body.style.backgroundColor = newChangeColor;
+  txtColor.textContent = newChangeColor;
+}
